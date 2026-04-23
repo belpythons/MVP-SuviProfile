2659bdcbbc3fc6476dacdc4715d81f1aa0046104
belpythons <zerobocilicung393@gmail.com>
2026-04-23 10:42:01 +0800

    chore: track .next build folder for manual domcloud deployment

---
+++ .next/server/chunks/ssr/_next-internal_server_app_admin_courses_page_actions_0k-awkv.js
@@ -0,0 +1,3 @@
+module.exports=[95147,a=>{"use strict";var b=a.i(26435);a.s([],95930),a.i(95930),a.s(["00b1157180f7a9aa1d10f519490c670eb13261c434",()=>b.$$RSC_SERVER_ACTION_0],95147)}];
+
+//# sourceMappingURL=_next-internal_server_app_admin_courses_page_actions_0k-awkv.js.map
---
+++ .next/server/chunks/ssr/_next-internal_server_app_admin_dashboard_page_actions_0jb_3m..js
@@ -0,0 +1,3 @@
+module.exports=[77735,a=>{"use strict";var b=a.i(26435);a.s([],92067),a.i(92067),a.s(["00b1157180f7a9aa1d10f519490c670eb13261c434",()=>b.$$RSC_SERVER_ACTION_0],77735)}];
+
+//# sourceMappingURL=_next-internal_server_app_admin_dashboard_page_actions_0jb_3m..js.map
